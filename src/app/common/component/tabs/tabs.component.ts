153bc37aec6108b53ed38fb8757e8db20adb3c8b
abhilashmaipady <abhilashmaipady@gmail.com>
2019-11-13 02:43:22 +0530

    intermediate commit

--- src/app/common/component/tabs/tabs.component.ts
+++ src/app/common/component/tabs/tabs.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ContentChildren, AfterViewInit } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterViewInit {
 
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
   }
 
 }
