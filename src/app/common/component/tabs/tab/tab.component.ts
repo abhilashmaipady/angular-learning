57cc442530a58583e4c313a17c654abc0c8e1325
abhilashmaipady <abhilashmaipady@gmail.com>
2019-11-21 02:30:11 +0530

    Adding viewchild

--- src/app/common/component/tabs/tab/tab.component.ts
+++ src/app/common/component/tabs/tab/tab.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit, Input, TemplateRef, ElementRef, ViewContainerRef, ContentChild } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef, ViewChild, ChangeDetectionStrategy, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
-  templateUrl: './tab.component.html'
+  templateUrl: './tab.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TabComponent implements OnInit {
+export class TabComponent implements OnInit, AfterViewInit {
 
   @Input() tabId !: string;
   @Input() tabHeader !: string;
-  @Input() content: TemplateRef<any>;
-  constructor(private viewContainerRef: ViewContainerRef) {
+  @ViewChild('templateRef', { static: true }) content: TemplateRef<any>;
+  constructor(private changeDetector: ChangeDetectorRef) {
     // console.log(elementRef);
     // this.content = viewContainerRef.element;
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.changeDetector.detectChanges();
   }
 
 }
